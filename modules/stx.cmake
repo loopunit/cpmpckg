85e10aab978894186b222113035dc3a2cc8580fd
Nathan Rausch <1158984+loopunit@users.noreply.github.com>
2021-01-27 21:59:54 -0800

    Added stx

--- modules/stx.cmake
+++ modules/stx.cmake
@@ -1,3 +1,11 @@
+add_cpm_module(stx FOR_TOOLCHAIN)
+
+add_library(cpm_toolchain::stx STATIC IMPORTED)
+target_include_directories(cpm_toolchain::stx INTERFACE ${stx_ROOT}/include)
+set_target_properties(cpm_toolchain::stx PROPERTIES IMPORTED_LOCATION ${stx_ROOT}/lib/stx.lib)
+
+####
+
 add_cpm_module(stx)
 
 add_library(cpm_runtime::stx STATIC IMPORTED)
