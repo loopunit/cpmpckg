0acc36fb6dcfdd8c5a9795d8798282af17aa2bb2
Nathan Rausch <1158984+loopunit@users.noreply.github.com>
2021-01-28 21:52:15 -0800

    update imgui_app, re-enable example

--- modules/imgui_app.cmake
+++ modules/imgui_app.cmake
@@ -4,6 +4,6 @@ CPMAddBaseModule(imgui_addons)
 add_cpm_module(imgui_app)
 
 add_library(cpm_runtime::imgui_app STATIC IMPORTED)
-target_include_directories(cpm_runtime::imgui_app INTERFACE ${imgui_addons_ROOT}/include)
-set_target_properties(cpm_runtime::imgui_app PROPERTIES IMPORTED_LOCATION ${imgui_addons_ROOT}/lib/imgui_app.lib)
+target_include_directories(cpm_runtime::imgui_app INTERFACE ${imgui_app_ROOT}/include)
+set_target_properties(cpm_runtime::imgui_app PROPERTIES IMPORTED_LOCATION ${imgui_app_ROOT}/lib/imgui_app.lib)
 target_link_libraries(cpm_runtime::imgui_app INTERFACE cpm_runtime::imgui_addons cpm_runtime::imgui_app_fw)
