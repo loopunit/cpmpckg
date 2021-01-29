c9a0792128f6c2cc0e4861c18ea56cd801d1c1aa
Nathan Rausch <1158984+loopunit@users.noreply.github.com>
2021-01-28 21:05:40 -0800

    Added imgui_addons module

---
+++ modules/imgui_addons.cmake
@@ -0,0 +1,5 @@
+add_cpm_module(imgui_addons)
+
+add_library(cpm_runtime::imgui_addons STATIC IMPORTED)
+target_include_directories(cpm_runtime::imgui_addons INTERFACE ${imgui_addons_ROOT}/include)
+set_target_properties(cpm_runtime::imgui_addons PROPERTIES IMPORTED_LOCATION ${imgui_addons_ROOT}/lib/imgui_addons.lib)
