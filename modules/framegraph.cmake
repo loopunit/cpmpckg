c5770d95014b65b31486ddb9b43ddd8c421beb96
Nathan Rausch <1158984+loopunit@users.noreply.github.com>
2021-01-30 00:40:37 -0800

    Cleanup

--- modules/framegraph.cmake
+++ modules/framegraph.cmake
@@ -1,4 +1,6 @@
-if(NOT TARGET cpm_runtime::framegraph_framework OR NOT TARGET cpm_runtime::framegraph_vulkanloader OR NOT TARGET cpm_runtime::framegraph_stl)
+if(NOT TARGET cpm_runtime::framegraph_framework OR NOT TARGET cpm_runtime::framegraph_vulkanloader OR NOT TARGET cpm_runtime::framegraph_stl OR NOT TARGET cpm_runtime::framegraph_pipelinecompiler OR NOT TARGET cpm_runtime::framegraph)
+	CPMAddBaseModule(glslang)
+
 	add_cpm_module(framegraph)
 	add_library(cpm_runtime::framegraph_framework STATIC IMPORTED)
 	set_target_properties(cpm_runtime::framegraph_framework PROPERTIES IMPORTED_LOCATION ${framegraph_ROOT}/lib/Framework.lib)
@@ -8,9 +10,7 @@ if(NOT TARGET cpm_runtime::framegraph_framework OR NOT TARGET cpm_runtime::frame
 
 	add_library(cpm_runtime::framegraph_stl STATIC IMPORTED)
 	set_target_properties(cpm_runtime::framegraph_stl PROPERTIES IMPORTED_LOCATION ${framegraph_ROOT}/lib/STL.lib)
-endif()
 
-if(NOT TARGET cpm_runtime::framegraph_pipelinecompiler OR NOT TARGET cpm_runtime::framegraph)
 	add_library(cpm_runtime::framegraph_pipelinecompiler STATIC IMPORTED)
 	set_target_properties(cpm_runtime::framegraph_pipelinecompiler PROPERTIES IMPORTED_LOCATION ${framegraph_ROOT}/lib/PipelineCompiler.lib)
 
@@ -20,7 +20,5 @@ if(NOT TARGET cpm_runtime::framegraph_pipelinecompiler OR NOT TARGET cpm_runtime
 	target_include_directories(cpm_runtime::framegraph INTERFACE ${framegraph_ROOT}/include/vulkan_loader/1.1)
 	target_compile_definitions(cpm_runtime::framegraph INTERFACE FG_ENABLE_VULKAN=1 FG_ENABLE_VULKAN_MEMORY_ALLOCATOR=1 COMPILER_MSVC=1 FG_STD_VARIANT=1 FG_STD_OPTIONAL=1 FG_STD_STRINGVIEW=1 FG_CACHE_LINE=std::hardware_destructive_interference_size FG_HAS_HASHFN_HashArrayRepresentation=1 FG_HAS_HASHFN_HashBytes=1 FG_HAS_HASHFN_Murmur2OrCityhash=1 FG_VULKAN_TARGET_VERSION="110")
 	target_link_libraries(cpm_runtime::framegraph INTERFACE cpm_runtime::framegraph_framework cpm_runtime::framegraph_vulkanloader cpm_runtime::framegraph_stl cpm_runtime::framegraph_pipelinecompiler)
-
-	CPMAddBaseModule(glslang)
 	target_link_libraries(cpm_runtime::framegraph INTERFACE cpm_runtime::glslang cpm_runtime::spirv)
 endif()
--- modules/leaf.cmake
+++ modules/leaf.cmake
@@ -1,7 +1,7 @@
 if(NOT TARGET cpm_toolchain::leaf)
 	add_cpm_module(leaf FOR_TOOLCHAIN)
 
-	add_library(cpm_toolchain::leaf STATIC IMPORTED)
+	add_library(cpm_toolchain::leaf INTERFACE)
 	target_include_directories(cpm_toolchain::leaf INTERFACE ${leaf_ROOT}/include)
 endif()
 
@@ -10,6 +10,6 @@ endif()
 if(NOT TARGET cpm_runtime::leaf)
 	add_cpm_module(leaf)
 
-	add_library(cpm_runtime::leaf STATIC IMPORTED)
+	add_library(cpm_runtime::leaf INTERFACE)
 	target_include_directories(cpm_runtime::leaf INTERFACE ${leaf_ROOT}/include)
 endif()
