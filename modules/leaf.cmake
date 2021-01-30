61af6e7afdf4084d8c01761d6af17d07fb7267fe
Nathan Rausch <1158984+loopunit@users.noreply.github.com>
2021-01-29 23:52:35 -0800

    Added leaf

---
+++ modules/leaf.cmake
@@ -0,0 +1,15 @@
+if(NOT TARGET cpm_toolchain::leaf)
+	add_cpm_module(leaf FOR_TOOLCHAIN)
+
+	add_library(cpm_toolchain::leaf STATIC IMPORTED)
+	target_include_directories(cpm_toolchain::leaf INTERFACE ${leaf_ROOT}/include)
+endif()
+
+####
+
+if(NOT TARGET cpm_runtime::leaf)
+	add_cpm_module(leaf)
+
+	add_library(cpm_runtime::leaf STATIC IMPORTED)
+	target_include_directories(cpm_runtime::leaf INTERFACE ${leaf_ROOT}/include)
+endif()
