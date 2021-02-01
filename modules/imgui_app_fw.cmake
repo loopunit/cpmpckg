d6af4a49dd6116b12418c57397854ae9a630f69e
Nathan Rausch <1158984+loopunit@users.noreply.github.com>
2021-01-31 19:09:50 -0800

    updated imgui_app_fw

--- modules/imgui_app_fw.cmake
+++ modules/imgui_app_fw.cmake
@@ -4,11 +4,12 @@ if(NOT TARGET cpm_runtime::imgui_app_fw)
 	CPMAddBaseModule(basis_universal)
 	CPMAddBaseModule(glfw)
 	CPMAddBaseModule(boostorg_leaf)
+	CPMAddBaseModule(mu_stdlib)
 
 	add_cpm_module(imgui_app_fw)
 
 	add_library(cpm_runtime::imgui_app_fw STATIC IMPORTED)
 	target_include_directories(cpm_runtime::imgui_app_fw INTERFACE ${imgui_app_fw_ROOT}/include)
-	target_link_libraries(cpm_runtime::imgui_app_fw INTERFACE cpm_runtime::imgui cpm_runtime::glfw cpm_runtime::framegraph cpm_runtime::basis_universal)
+	target_link_libraries(cpm_runtime::imgui_app_fw INTERFACE cpm_runtime::imgui cpm_runtime::glfw cpm_runtime::framegraph cpm_runtime::basis_universal cpm_runtime::mu_stdlib)
 	set_target_properties(cpm_runtime::imgui_app_fw PROPERTIES IMPORTED_LOCATION ${imgui_app_fw_ROOT}/lib/imgui_app_fw.lib)
 endif()
